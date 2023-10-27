2e8c09ef4fe3e6eb6b42f8c62316a0acf904e709
Mateusz Godlewski <thyen00@gmail.com>
2023-10-27 13:00:14 +0200

    adding solution

--- main.cpp
+++ main.cpp
@@ -3,24 +3,28 @@
 #include <vector>
 #include <ranges>
 
-auto reversedWords(std::string sentence) -> std::string {
-    std::ranges::reverse(sentence);
-    sentence += ' ';
-    for (auto wordBegin = sentence.begin();
-         wordBegin != sentence.end();) {
-        auto wordEnd = std::ranges::find(wordBegin, sentence.end(), ' ');
-        std::ranges::reverse(wordBegin, wordEnd);
-        wordBegin = wordEnd + 1;
-    }
-    return sentence;
-}
+auto example(
+        std::vector<std::string> &left, std::vector<std::string> &right
+) -> void {
+    auto eraseDuplicates = [](std::vector<std::string> &vec) {
+        std::ranges::sort(vec);
+        auto duplicates = std::ranges::unique(vec);
+        vec.erase(duplicates.begin(), duplicates.end());
+    };
 
-auto main() -> int {
-    fmt::println(
-            "{}\n{}\n{}\n{}\n",
-            reversedWords("Ala ma kota"),
-            reversedWords("Ala"),
-            reversedWords(""),
-            reversedWords("1 2 3")
-    );
+    auto customSort = [](std::vector<std::string> &vec) {
+        std::ranges::sort(vec, [](const std::string& a, const std::string& b) {
+            if ((a.front() >= '0' && a.front() <= '9' && b.front() >= '0' && b.front() <= '9') ||
+                (a.front() < '0' && b.front() < '0') || (a.front() >= 'A' && b.front() >= 'A')) {
+                return a < b;
+            }
+            return a.front() >= '0' && a.front() <= '9';
+        });
+    };
+
+    eraseDuplicates(left);
+    eraseDuplicates(right);
+
+    customSort(left);
+    customSort(right);
 }
